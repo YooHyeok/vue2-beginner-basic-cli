fdf380a68d23ff7f07a40a179d36ff7bc5636d3c
webdevyoo <yjk7454@naver.com>
2024-10-11 01:07:17 +0900

    feat: `AboutView.vue` - YooHyeokSchool 컴포넌트 참조 및 선언 (재사용)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <YooHyeokSchool/>
+
   </div>
 </template>
+<script>
+import YooHyeokSchool from '@/components/YooHyeokSchool.vue'
+
+export default {
+  components: { YooHyeokSchool },
+  data() {
+    return {
+      name: 'YooHyeok School'
+    }
+  },
+  methods: {
+    updateName() {
+      this.name = 'YooHyeok School Updated'
+    }
+  }
+}
+</script>
