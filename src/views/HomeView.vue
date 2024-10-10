d4ca220d53f6450d01b898e46b9e71d9939e963a
webdevyoo <yjk7454@naver.com>
2024-10-11 01:06:38 +0900

    feat: `HomeView.vue` - YooHyeokSchool 컴포넌트 참조 및 선언(케밥-표기법)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,13 @@
     <h1>This is a Home page</h1>
     <p>{{ name }}</p>
     <button @click="updateName">Change Name</button>
+    <yoo-hyeok-school/>
   </div>
 </template>
 <script>
+import YooHyeokSchool from '@/components/YooHyeokSchool.vue'
 export default {
+  components: { YooHyeokSchool },
   data() {
     return {
       name: 'YooHyeok School'
