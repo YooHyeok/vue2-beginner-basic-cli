889223cd64ee6025426d36e474df56bd5a4200f2
webdevyoo <yjk7454@naver.com>
2024-10-11 01:04:22 +0900

    feat: `YooHyeokSchool.vue` - 버튼클릭 name state 제어 및 출력 컴포넌트화

---
+++ src/components/YooHyeokSchool.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <button @click="updateName">Change Name</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'YooHyeokSchool',
+  data() {
+    return {
+      name: 'YooHyeok School'
+    }
+  },
+  methods: {
+    updateName() {
+      this.name = 'YooHyeok School Updated'
+    }
+  }
+  
+}
+</script>
+<style scoped>
+
+</style>
