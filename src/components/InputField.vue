26f654d562b2c98c2c397430be998554acead544
webdevyoo <yjk7454@naver.com>
2024-10-13 00:57:18 +0900

    feat: `InputField.vue` - emit 예제 컴포넌트 추가 및 $emit 간접 호출

---
+++ src/components/InputField.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label for="">Name</label>
+    <input 
+      type="text" 
+      :value="name" 
+      style="padding: 30px; border: 2px solid green"
+      @input=" changeName"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: null
+    },
+  },
+  methods: {
+    changeName(e) {
+      this.$emit('changeName', e.target.value)
+    }
+  }
+}
+</script>
