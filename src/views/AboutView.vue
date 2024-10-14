ff709734c6876a38d1b3adb2b1c4d25ad7811090
webdevyoo <yjk7454@naver.com>
2024-10-15 00:51:05 +0900

    feat: `AboutView.vue` - <slot> 대응 (header, default, footer 및 props)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,26 @@
   <div class="about">
     <h1>This is an about page</h1>
     <YooHyeokSchool v-bind="props"/>
-
+    <YooHyeokSchool>
+      <template v-slot:header="props"> <!-- 축약형: #header, slot의 props 바인딩 가능 -->
+        <div>
+          <h1>Header! {{ props.nameAttr }}</h1>
+          <p>header!</p>
+        </div>
+      </template>
+      <template v-slot:footer="{ nameAttr }"> <!-- 축약형: #footer -->
+        <div>
+          <h1>Footer! {{ nameAttr }}</h1>
+          <p>footer!</p>
+        </div>
+      </template>
+      <template> <!-- v-slot:default 생략 가능 -->
+        <div>
+          <h1>Nomal!</h1>
+          <p>nomal!</p>
+        </div>
+      </template>
+    </YooHyeokSchool>
   </div>
 </template>
 <script>
