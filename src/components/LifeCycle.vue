5bbc1b728d7462af39820b8f0be3a1154cfe0a24
webdevyoo <yjk7454@naver.com>
2024-10-16 00:17:01 +0900

    feat: `LifeCycle.vue` - 뷰 라이프사이클 훅 예제 컴포넌트 추가

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <br>
+    {{ name }}
+    <button @click="updateName">updateName</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name:'LifeCycle'
+    }
+  },
+  beforeCreate() {console.log("beforeCreate", this.name)},
+  created() {console.log("created", this.name)},
+  beforeMount() {console.log("beforeMount", this.name)},
+  mounted() {
+    console.log("mounted", this.name)
+    this.updateName();
+  },
+  beforeUpdate() {console.log("beforeUpdate", this.name)},
+  updated() {console.log("updated", this.name)},
+  beforeDestroy() {console.log("beforeDestroy", this.name)}, // about 컴포넌트로 route할 경우 실행됨
+  destroyed() {console.log("destroyed", this.name)},
+  methods: {
+    updateName() {
+      this.name='LifeCycle: '
+    }
+  }
+}
+</script>
