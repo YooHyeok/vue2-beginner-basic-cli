90cd91e983fdb949dcd8035c01bfb62c888d846d
webdevyoo <yjk7454@naver.com>
2024-10-16 00:41:28 +0900

    feat: `HomeView.vue` - Vue.js의 이벤트 핸들링, $event 객체 관련 주석 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@
       <!-- <InputField :name="name" @changeName="changeName"/>  --> <!-- $emit으로 넘긴 값을 간접적으로 받을 경우 값에 해당하는 $event를 해당 함수의 인자로 자동 전달 -->
       <!-- <InputField :name="name" @changeName="name=$event"/> --> <!-- $emit으로 넘긴 값을 직접적으로 받을 경우 $event로 바로 받는다. -->
       <InputField v-model="name"/> <!-- v-model은 내부적으로 @input="name=$event"와 :value="name"이 적용되므로 emit에서 input을 호출한다. (props가 value로 넘어감) -->
+      <!-- @input="name=$event"는 @input="(e) => {name = e}" 와 같음-->
       <button>Submit</button>
     </form>
     <h1>This is a Home page</h1>
